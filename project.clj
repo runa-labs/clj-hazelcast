5d1f7845030b851ab47a0b69aceaa62a104cba34
Ram Krishnan <kriyative@gmail.com>
2012-11-14 19:07:33 -0800

    Release 1.0.1.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.runa/clj-hazelcast "1.0.0"
+(defproject com.runa/clj-hazelcast "1.0.1"
   :description "Clojure library for the Hazelcast p2p cluster"
   :plugins [[s3-wagon-private "1.1.2"]
             [lein-swank "1.4.4"]]
