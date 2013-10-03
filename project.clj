8282e9318d085695a22dd2d9fd6703dae99b5201
Kasim Tuman <ktuman@gmail.com>
2013-10-02 18:59:11 -0700

    Updated to clojure 1.5.1 and clj-kryo 1.3.0

--- project.clj
+++ project.clj
@@ -1,8 +1,6 @@
 (defproject com.runa/clj-hazelcast "1.0.1"
   :description "Clojure library for the Hazelcast p2p cluster"
-  :plugins [[s3-wagon-private "1.1.2"]
-            [lein-swank "1.4.4"]]
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [com.runa/clj-kryo "1.2.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojars.runa/clj-kryo "1.3.0"]
                  [com.hazelcast/hazelcast "2.4"]]
   :repositories {"releases" {:url "s3p://runa-maven/releases/"}})
