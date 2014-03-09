aec083dd3b17afb7993ed0e936448cf77d6e1e2c
Rui Carmo <rui.carmo@gmail.com>
2014-03-09 18:57:46 +0000

    upgraded deps

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject com.runa/clj-hazelcast "1.0.1"
   :description "Clojure library for the Hazelcast p2p cluster"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojars.runa/clj-kryo "1.3.0"]
-                 [com.hazelcast/hazelcast "3.1.3"]]
+                 [org.clojars.runa/clj-kryo "1.4.1"]
+                 [com.hazelcast/hazelcast "3.1.6"]]
   :repositories {"releases" {:url "s3p://runa-maven/releases/"}})
