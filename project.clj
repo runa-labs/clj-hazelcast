bbe2502658484cef8f25ec68b8bd9a0ab32c2e9a
Halit Olali <halitolali@gmail.com>
2014-03-21 19:33:23 +0200

    hz 3.2 upgrade & playground

--- project.clj
+++ project.clj
@@ -2,5 +2,6 @@
   :description "Clojure library for the Hazelcast p2p cluster"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojars.runa/clj-kryo "1.4.1"]
-                 [com.hazelcast/hazelcast "3.1.6"]]
+                 [com.hazelcast/hazelcast "3.2-RC2"]
+                 ]
   :repositories {"releases" {:url "s3p://runa-maven/releases/"}})
---
+++ src/clj_hazelcast/mr.clj
@@ -0,0 +1,2 @@
+(ns clj-hazelcast.mr)
+
