ac5db1e532e81154401afa6461efb5dea9b2fd3e
Halit Olali <halitolali@gmail.com>
2014-03-25 21:38:07 +0200

    use latest clojure release

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject com.runa/clj-hazelcast "1.0.1"
   :description "Clojure library for the Hazelcast p2p cluster"
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojars.runa/clj-kryo "1.4.1"]
                  [com.hazelcast/hazelcast "3.2-RC2"]
                  [org.clojure/tools.logging "0.2.6"]
