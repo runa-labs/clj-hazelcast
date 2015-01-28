3cc8a31e8d86fb9d7cec8e766716aa3894886702
Ram Krishnan <kriyative@gmail.com>
2015-01-28 12:41:21 -0800

    Updated clj-kryo and hazelcast dependencies.

--- project.clj
+++ project.clj
@@ -1,11 +1,9 @@
-(defproject com.runa/clj-hazelcast "1.0.1"
+(defproject com.runa/clj-hazelcast "1.2.0"
   :description "Clojure library for the Hazelcast p2p cluster"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojars.runa/clj-kryo "1.4.1"]
-                 [com.hazelcast/hazelcast "3.3"]
-                 ]
+                 [org.clojars.runa/clj-kryo "1.5.0"]
+                 [com.hazelcast/hazelcast "3.4"]]
   :profiles {:provided {:dependencies [[org.clojure/tools.logging "0.2.6"]]}}
   :global-vars {*warn-on-reflection* true}
   :min-lein-version "2.0.0" ;provided deps
-  :java-source-paths ["src-java"]
-  )
+  :java-source-paths ["src-java"])
