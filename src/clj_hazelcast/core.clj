ef62f3cb9b7ea7647b22f5353800d8c02f0b8798
David Dossot <david@dossot.net>
2015-02-26 17:19:27 -0800

    fixed import that had been lost in merge

--- src/clj_hazelcast/core.clj
+++ src/clj_hazelcast/core.clj
@@ -7,7 +7,7 @@
    (com.hazelcast.config XmlConfigBuilder Config TcpIpConfig)
    (java.util Collection Set List Map HashSet Queue)
    java.util.concurrent.locks.Lock
-   java.util.concurrent.BlockingQueue))
+   [java.util.concurrent BlockingQueue TimeUnit]))
 
 (def ^:dynamic hazelcast (atom nil))
 
